b14f651b159e4af8ba1b85deadec65d0723f2e66
Icaro-Lima <icaro.lima@ccc.ufcg.edu.br>
2018-09-30 20:59:26 -0300

    Add BST

--- src/icarolima/BST/BSTImpl.java
+++ src/icarolima/BST/BSTImpl.java
@@ -1,19 +1,58 @@
 package icarolima.BST;
 
-public class BSTImpl<T> {
+import icarolima.common.Drawable;
+
+public class BSTImpl<T extends Comparable<T>> implements Drawable {
 	
 	private T value;
 	
+	private BSTImpl<T> left;
+	private BSTImpl<T> right;
+	
 	public BSTImpl(T value) {
-		this.setValue(value);
-	}
-
-	public T getValue() {
-		return value;
-	}
-
-	public void setValue(T value) {
 		this.value = value;
+	}
+	
+	public void add(T value) {
+		if (value.compareTo(this.value) <= -1) {
+			this.addOnLeft(value);
+		} else {
+			this.addOnRight(value);
+		}
+	}
+	
+	private void addOnLeft(T value) {
+		if (this.left == null) {
+			this.left = new BSTImpl<T>(value);
+		} else {
+			this.left.add(value);
+		}
+	}
+	
+	private void addOnRight(T value) {
+		if (this.right == null) {
+			this.right = new BSTImpl<T>(value);
+		} else {
+			this.right.add(value);
+		}
+	}
+	
+	public T value() {
+		return this.value;
+	}
+
+	public BSTImpl<T> getLeft() {
+		return this.left;
+	}
+
+	public BSTImpl<T> getRight() {
+		return this.right;
+	}
+
+	@Override
+	public String draw() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
---
+++ src/icarolima/main/Main.java
@@ -0,0 +1,12 @@
+package icarolima.main;
+
+import icarolima.BST.BSTImpl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BSTImpl<Integer> bst = new BSTImpl<>(5);
+		bst.add(3);
+	}
+	
+}
